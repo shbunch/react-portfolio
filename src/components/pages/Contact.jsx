0f1ded73eb7d795f2fc025cf1515a9ca2bc0254f
shbunch <shawnbunch@gmail.com>
2023-08-25 22:50:29 -0500

    Updated Contact page with form

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,13 +1,47 @@
-export default function Contact() {
+import React from 'react'
+
+const Contact = () => {
+  const [formStatus, setFormStatus] = React.useState('Send')
+  const onSubmit = (e) => {
+    e.preventDefault()
+    setFormStatus('Submitting...')
+    const { name, email, message } = e.target.elements
+    let conFom = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    }
+    console.log(conFom)
+  }
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <label>Name</label>
-      <input/> <br></br>
-      <label>Email</label>
-      <input/> <br></br>
-      <label>Message</label>
-      <input/>
+    <div className="container mt-5">
+      <h2 className="text-center mb-3">Contact Me!</h2>
+      <form onSubmit={onSubmit}>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="name">
+            Name
+          </label>
+          <input className="form-control" type="text" id="name" required />
+        </div>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="email">
+            Email
+          </label>
+          <input className="form-control" type="email" id="email" required />
+        </div>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="message">
+            Message
+          </label>
+          <textarea className="form-control" id="message" required />
+        </div>
+        <div class="text-center">
+          <button className="sendbutton btn btn-danger" id="sendButton" type="submit">
+            {formStatus}
+          </button>
+        </div>
+      </form>
     </div>
-  );
+  )
 }
+export default Contact
