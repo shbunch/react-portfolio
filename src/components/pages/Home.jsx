dad6e9736164f724f7e0025ab3dc317c9efc0d58
shbunch <shawnbunch@gmail.com>
2023-08-25 22:13:43 -0500

    Added avatar and more animation and styling

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,7 +4,12 @@ export default function Home() {
   return (
     <div id='hero-container'>
       <div id="hero">
-      </div>
+      <img
+        src="../public/images/SB.jpg"
+        alt="shawnbunch"
+        id="avatar"
+      />
+      </div>   
       <h1 id="welcome">Shawn Bunch
         <p>Full Stack Web Developer</p>
         <p>Welcome to my page!</p>
